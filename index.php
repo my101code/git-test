da427c3587150869bcc2bbfc12e243be91e196a8
Moha Lhabdellaui <my101code@gmail.com>
2025-06-24 12:21:32 +0100

    añadido un form para contacto

--- index.php
+++ index.php
@@ -6,5 +6,12 @@
   </head>
   <body>
     <h1 class="bg-white">Bienvenido a mi sitio</h1>
+    <div>
+        <!-- formulario añadido -->
+        <form action="/contactar">
+          <input type="text" name="nombre" placeholder="Tu nombre" />
+          <button type="submit">Enviar</button>
+        </form>
+    </div>
   </body>
 </html>
